7f505f85d46b8c53a6ef86634c3bf1be4827c469
Aliu <liuyanxia@hydee.cn>
2026-03-26 17:30:49 +0800

    migrate frontend to vue vite

---
+++ frontend/src/components/PlatformTile.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="tile"
+    :data-platform="platform"
+    :data-feature="platform"
+    :class="{
+      'is-loading': isLoading,
+      'is-disabled': isDisabled,
+      'is-ready': hasContent && !isLoading
+    }"
+    @click="onClick"
+  >
+    <div class="tile-icon" :class="iconClass">
+      <img :src="icon" :alt="title" />
+    </div>
+
+    <h3>{{ title }}</h3>
+
+    <p class="tile-hint">
+      {{ hintText }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  platform: String,
+  title: String,
+  icon: String,
+  iconClass: String,
+  hintText: String,
+  isLoading: Boolean,
+  isDisabled: Boolean,
+  hasContent: Boolean
+})
+
+const emit = defineEmits(['tile-click'])
+
+function onClick() {
+  emit('tile-click', props.platform)
+}
+</script>
